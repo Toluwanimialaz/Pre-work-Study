9eb63fd9c9ce12a19b38e199ee6cc19bb83ac803
Toluwanimialaz <toluwanimiala28@gmail.com>
2023-03-08 14:54:02 +0100

    function code added to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,26 @@
-/*var topic;
-if(0){
-    topic="html";
-} else if(null){
-    topic="css";
-} else if(false){
-    topic="git";
-} else if(true){
-    topic="js";
-} else{
-    topic="none ";
-}*/
-
 var topics=["HTML","CSS","Git","Javascript"]
+var rand=Math.floor(Math.random()*topics.length);
 
-for(i=0;i<topics.length;i++){
-    console.log(topics[i]);
+function br(){
+    console.log("which topic should we study first")
+
+    for(i=0;i<topics.length;i++){
+        if(topics[rand]=="HTML"){
+            console.log("lets study html");
+        } else if(topics[rand]=="CSS"){
+            console.log("lets study CSS");
+        } else if(topics[rand]=="Git"){
+            console.log("lets study git");
+        } else if(topics[rand]=="Javascript"){
+            console.log("lets study Javascript");
+        } else{
+            console.log("none");
+        }
+    }
+
+    
 }
+
+br()
+   
 
